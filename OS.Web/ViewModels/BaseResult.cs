9d9e29d46581444f035c51b491870e668119b115
ashan <madu2000official@gmai.com>
2018-04-07 22:18:27 +0530

    add user controller

---
+++ OS.Web/ViewModels/BaseResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OS.Web.ViewModels
+{
+    public class BaseResponse
+    {
+        public bool Succeeded { get; set; }
+        public string Message { get; set; }
+    }
+}
