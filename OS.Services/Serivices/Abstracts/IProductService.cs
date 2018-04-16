68dc27d832bdbcfa43b72a771eaf545c16fc0b05
ashan <madu2000official@gmai.com>
2018-04-16 20:36:16 +0530

    add prodcut service/controller

---
+++ OS.Services/Serivices/Abstracts/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using OS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OS.Services.Serivices.Abstracts
+{
+    public interface IProductService
+    {
+        IEnumerable<Product> GetProductListForProductType(int productTypeId);
+    }
+}
