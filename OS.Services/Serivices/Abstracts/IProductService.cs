51580a005faae414bbe36319c77c2e37d240b0c4
ashan <madu2000official@gmai.com>
2018-04-18 20:04:53 +0530

    add services

--- OS.Services/Serivices/Abstracts/IProductService.cs
+++ OS.Services/Serivices/Abstracts/IProductService.cs
@@ -2,11 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace OS.Services.Serivices.Abstracts
 {
     public interface IProductService
     {
         IEnumerable<Product> GetProductListForProductType(int productTypeId);
+        Task<IEnumerable<Product>> GetProductListForCategoryPage();
+        Product GetSingleProduct(int id);
     }
 }
--- OS.Services/Serivices/ProductService.cs
+++ OS.Services/Serivices/ProductService.cs
@@ -4,22 +4,39 @@ using OS.Services.Serivices.Abstracts;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace OS.Services.Serivices
 {
     public class ProductService : IProductService
     {
+        #region fields
         private readonly IProductRepository _productRepository;
+        #endregion
 
+        #region constructor
         public ProductService(IProductRepository productRepository)
         {
             this._productRepository = productRepository;
         }
+        #endregion
 
+        #region Methods
         public IEnumerable<Product> GetProductListForProductType(int productTypeId)
         {
             var _product = _productRepository.FindBy(p => p.ProductTypeId == productTypeId);
             return _product;
         }
+
+        public Task<IEnumerable<Product>> GetProductListForCategoryPage()
+        {
+            return _productRepository.FindByAsync(p => p.ShowOnCategoryPage == 1);
+        }
+
+        public Product GetSingleProduct(int id)
+        {
+            return _productRepository.GetSingle(id);
+        }
+        #endregion 
     }
 }
