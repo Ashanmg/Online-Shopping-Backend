69c2bb8009d02f8b669ce1d46d1b687cc6976821
ashan <madu2000official@gmai.com>
2018-05-27 16:29:18 +0530

    add cart details services and methods

--- OS.Web/ViewModels/ShoppingCartItemModel.cs
+++ OS.Web/ViewModels/ShoppingCartItemModel.cs
@@ -7,6 +7,14 @@ namespace OS.Web.ViewModels
 {
     public class ShoppingCartItemModel
     {
-
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ColourId { get; set; }
+        public int SizeId { get; set; }
+        public int ProductId { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int Quantity { get; set; }
+        public int StoreId { get; set; }
+        public int ShoppingCartTypeId { get; set; }
     }
 }
